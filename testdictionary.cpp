bdd92e50f07ff1e09d408ebc4fbd756c98b05199
Himanshu Singal <singal_himanshu@yahoo.co.in>
2015-04-14 12:08:44 +0530

    testdictionary.cpp commented

--- testdictionary.cpp
+++ testdictionary.cpp
@@ -21,17 +21,17 @@ int main()
       case 1:
 	std::cout<<"Input record:\nItem_Num Item_Name Remaining_Num Price Manufacturing_Company Manufacturing_addres\n";
 	a.input();
-	if(!data.insert(a))
+	if(!data.insert(a))     // inserting in the dictionary
 	  {
 	    std::cout<<"Table Full - Rehashing the table ...\n";
-      	    data.assign(flag);
+      	    data.assign(flag);   // reassigning memory
 	  }
-	data.modifyFile();
+	data.modifyFile();      // updates the database.txt as per dictionary in memory
 	break;
       case 2:
 	std::cout<<"Enter Key\n";
 	std::cin>>key;
-	if(data.search(key))
+	if(data.search(key))       //search for a key
 	  std::cout<<"Found!!\n";
 	else
 	  std::cout<<"Not Found!!\n";
@@ -39,15 +39,15 @@ int main()
       case 3:
 	std::cout<<"Enter Key\n";
 	std::cin>>key;
-	if(data.delete1(key))
+	if(data.delete1(key))     //deletes a key
 	  std::cout<<"Deleted Successfully!!\n";
 	else
 	  std::cout<<"Not Found!!\n";
 	break;
       case 4:
-	std::cout<<"Enter Key\n";
+	std::cout<<"Enter Key\n";   
 	std::cin>>key;
-	if(!data.print(key))
+	if(!data.print(key))     //prints a data when its key is entered
 	  std::cout<<"Not Found!!\n";
 	break;
       }
